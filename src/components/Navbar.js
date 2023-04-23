d102a07d1ed11393814e64bebdc92dce9699caae
J-wrk <warakornbuachum@gmail.com>
2023-04-23 14:58:29 +0700

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ export class Navbar extends Component {
     render = () => (
         <div className="col-12">
             <h2 className="bg-primary text-while text-center p2">
-                {this.props.name} To do list
+                {this.props.name} TO DO LIST
             </h2>
         </div>
     );
