81fb9141a2ae826ede8b0378ddc850dab7252560
J-wrk <warakornbuachum@gmail.com>
2023-04-30 23:41:15 +0700

    Update Comment

--- src/components/TodoRows.js
+++ src/components/TodoRows.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 export class TodoRows extends Component {
+  // โชว์แถวขึ้นที่หน้าเว็บ โดยให้อ้างอืงจากค่าของ action ใน item ที่รับมา
+  // สร้าง checkbox ถ้ามีการเช็คให้ไปเปลี่ยนแปลงค่าของ done 
+  // ส่ง item ที่รับมา ไปที่ callback function
     render = () => (
       <tr>
-        <td>{this.props.item.action}</td>
+        <td>{this.props.item.action}</td> 
         <td>
           <input 
             type="checkbox"
